b68d2afd7a04bee492caad767123e83b616ea89a
sallliisa <sodiumcation@gmail.com>
2023-01-03 14:39:08 +0700

    light mode fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,23 @@ import type { AppProps } from 'next/app'
 import Master from '../components/Master'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Master>
-      <Header/>
-      <Component {...pageProps} />
-      <Footer/>
-    </Master>
+    <>
+      <Head>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+        <meta name='color-scheme' content='only light' />
+        <title>Say It!</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Master>
+        <Header/>
+        <Component {...pageProps} />
+        <Footer/>
+      </Master>
+    </>
   )
 }
 

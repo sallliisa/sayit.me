f618a76e1d8c2abe9b6e557fd2762fb1423b4815
sallliisa <sodiumcation@gmail.com>
2023-01-03 15:53:48 +0700

    changed favicon

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,7 +6,6 @@ interface ButtonProps {
 
 const Button = (props: ButtonProps) => {
     const {children, action, disabled=false} = props
-    console.log(disabled)
     return (
         <button disabled={disabled} onClick={disabled ? () => {} : (action)} className={`text-white font-bold py-2 px-4 rounded-md min-w-full bg- ${disabled ? `bg-neutral-800 hover:bg-neutral-800 cursor-not-allowed` : `bg-blue-500 hover:bg-blue-700`}`}>
             <div className="flex flex-row gap-2 items-center justify-center">
